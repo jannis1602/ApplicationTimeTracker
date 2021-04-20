8159e4407044eeb083a725cf412f6f4c5afa4116
Jannis <mattlage.jannis@gmail.com>
2021-04-20 19:06:24 +0200

    added scrollbar to gui better layout add filter menu

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Label, Button, Frame, Canvas, Scrollbar
+from tkinter import Tk, Label, Button, Frame, Canvas, Scrollbar, Entry
 
 
 class MainWindow():
@@ -10,28 +10,46 @@ class MainWindow():
 
         self.menu_bar_frame = Frame(
             master=root, height=20, width=640, bg="gray")
-        self.menu_bar_frame.pack(side='top', padx='5', pady='5')
-
+        self.menu_bar_frame.pack(side='top', padx='5',
+                                 pady='5', fill="x", expand=False)
+# String-Entry
+        string_entry = Entry(self.menu_bar_frame, bd=2, width=40)
+        string_entry.pack(side='left', padx='5', pady='5', expand=False)
+# Add-Button
+        self.greet_button = Button(
+            master=self.menu_bar_frame, text="Add to Filter", command=self.action)
+        self.greet_button.pack(side='left', padx='5', pady='5', expand=False)
+# Exit-Button
         self.greet_button = Button(
             master=self.menu_bar_frame, text="EXIT", command=self.action)
-        self.greet_button.pack(side='left', padx='5', pady='5')
+        self.greet_button.pack(side='right', padx='5', pady='5', expand=False)
+
+        # self.createWindowFrame(self.root) # test
 
 # ------------------------ list ------------------------
-        canvas = Canvas(self.root,bg="gray")
+
+        canvas = Canvas(self.root, bg="green")
         scroll_y = Scrollbar(self.root, orient="vertical",
                              command=canvas.yview)
-        scroll_frame = Frame(canvas)
+        scroll_frame = Frame(canvas,bg="blue")
         # for i in range(20):
-        #     Label(scroll_frame, text='label %i' % i).pack()
+        #     Label(scroll_frame, text='label %i' % i).pack()#
+
+        # temp_frame = Frame(
+        #     master=scroll_frame, height=20, width=400, bg="orange")
+        # temp_frame.pack(side='bottom', padx='5',
+        #                 pady='1', fill="x", expand=True)
         for i in range(20):
             self.createWindowFrame(scroll_frame)
+
         canvas.create_window(0, 0, anchor='nw', window=scroll_frame)
         canvas.update_idletasks()
-        canvas.configure(scrollregion=canvas.bbox('all'),
+        canvas.configure(scrollregion=canvas.bbox('all'),  # scrollregion=canvas.bbox('all') #TODO: #20 scrollregion
                          yscrollcommand=scroll_y.set)
         canvas.pack(fill='both', expand=True, side='left')
         scroll_y.pack(fill='y', side='right')
 
+        # scroll_frame.pack(fill='both', expand=True, side='left') # -> scrollbar funktioniert dann nicht!
 
         # self.list_frame = Frame(
         #     master=root, height=20, width=640, bg="gray")
@@ -44,8 +62,9 @@ class MainWindow():
 
     def createWindowFrame(self, master):
         temp_frame = Frame(
-            master=master, height=20, width=200, bg="gray")
-        temp_frame.pack(side='bottom', padx='5', pady='1',expand=True)
+            master=master, height=20, width=200, bg="red")
+        temp_frame.pack(side='bottom', padx=5,
+                        pady=1, fill="x", expand=False)
 
         name_label = Label(master=temp_frame, text="window", bg="gray")
         name_label.pack(padx=2, pady=2, side="left")
@@ -54,9 +73,13 @@ class MainWindow():
                            text="time: 40 Sekunden", bg="gray")
         name_label.pack(padx=2, pady=2, side="left")
 
+        # void_label = Label(master=temp_frame,
+        #                    text="  "*60, bg="gray")
+        # void_label.pack(padx=2, pady=2, side="left")
+
         btn = Button(temp_frame, text="remove",
                      bg="darkgray", command=self.action)
-        btn.pack(padx=2, pady=2, side="left")
+        btn.pack(padx=2, pady=2, side="right", fill="x")
 
         return btn
 
