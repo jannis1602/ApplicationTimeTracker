74a13c1f145da82885d59ab29ff7f11b1ef58cbd
Jannis <mattlage.jannis@gmail.com>
2021-04-22 12:13:02 +0200

    - checkListForElement methode - removed unused/old configWindow-methode

--- applicationTimeTracker.py
+++ applicationTimeTracker.py
@@ -86,81 +86,24 @@ def refresh():
     print("refresh")
 
 
-def checkIfFilterExists(nameString):
-    for w in windowList:
-        if w.windowName.lower() == nameString.lower():
+def checkListForElement(checkList, element, lower=True):
+    for e in checkList:
+        if lower and e.lower() == element.lower():
+            return True
+        elif lower == False and e == element:
             return True
     return False
 
 
 def addStringToFilter(name):
-    if len(name) > 1 and checkIfFilterExists(name) == False:
+    if len(name) > 1 and checkListForElement(windowList, name) == False:
         windowList.append(WindowObject(name))
 
 
-# def addToFilter():
-#     if len(entry_addString.get()) > 1:
-#         print("add", entry_addString.get(), "to Filter")
-#         addWindowName(str(entry_addString.get()))
-#         loadConfigStringList()
-#         entry_addString.delete(0, "end")
-
-
-# def hideConfigWindow():
-#     global configWindow
-#     configWindow.withdraw()
-
-
-# def loadConfigStringList():
-#     for w in windowList:
-#         global frame_configWindowList
-#         windowObjList.append(w.getConfigMenu(frame_configWindowList, 2+windowList.index(w),
-#                              lambda: remove(w.windowName)))
-
-
-# def remove(wname):
-#     print("remove...", wname)
-#     removeWindowName(wname)
-#     reload()
-
-
-# def showConfigWindow():
-#     global configWindow
-#     global frame_configWindowList
-#     global entry_addString
-#     configWindow = Tk()
-#     configWindow.title("ApplicationTimeTracker")
-#     configWindow.geometry('800x400')
-#     frame_configWindowList = Frame(configWindow)
-
-# # MenuBar
-#     menuBar = Frame(configWindow)
-#     button_change = Button(
-#         menuBar, text="Aktive Anwendungen (NoFunktion)", command=refresh)
-#     entry_addString = Entry(menuBar, bd=2, width=40)
-#     button_addString = Button(
-#         menuBar, text="add to Filter", command=addToFilter)
-#     button_reload = Button(
-#         menuBar, text="reload", command=reload)
-#     button_change.grid(row=0, column=0, padx=2)
-#     entry_addString.grid(row=0, column=1, padx=2)
-#     button_addString.grid(row=0, column=2, padx=2)
-#     button_reload.grid(row=0, column=3, padx=2)
-
-#     loadConfigStringList()
-
-#     menuBar.grid(row=0, column=0)
-#     frame_configWindowList.grid(row=1, column=0)
-
-#     configWindow.protocol("WM_DELETE_WINDOW", hideConfigWindow)
-#     configWindow.mainloop()
-
-
-def end():
+def end():  # TODO: rename
     print("EXIT...")
     global running
     running = False
-    # loopThread.join() # ???
 # ----> SAVE -- save save-list to database
     try:
         icon.visible = False
@@ -170,7 +113,7 @@ def end():
     except:
         pass
     mainWindow.stopRunning()
-    quit(0)  # or sys.exit(0) ?
+    quit(0)
 
 
 def showMainWindow():
@@ -190,27 +133,12 @@ def createMainWindow():
         target=mainWindow.createMainWindow(), name="mainWindow-Thread", daemon=True)
     mainWindowThread.start()
 
-# -----------------------sqlite-------------------
-# print('*'*100)
-# if(database.get_time_by_program_date("email", "2021-04-20") == None):
-#     database.add_program("email","2021-04-20",0)
-# database.add_time("email","2021-04-20",5)
-
-# database.delete_by_name("email")
-# print(database.get_times_by_program("email"))
-
-# if(database.get_time_by_program_date("Opera", "2021-04-20") == None):
-#     database.add_program("Opera","2021-04-20",0)
-# print(database.get_times_by_program("Opera"))
-# print(database.get_times_by_program("VScode"))
-# print('*'*100)
-
 # ------------------------------
 
 # database.add_program("Visual Studio Code")
 # database.add_program("Opera")
 
-# MainLoop ------------------------------------------------------------------
+# MainLoop --------------------------------------------
 
 
 def loop():
@@ -250,20 +178,12 @@ for w in windowList:
     print(w.getTimeString())
 print('*'*50)
 
-# temp: threadList
 # for thread in threading.enumerate():
 #     print(thread.name)
 
-
-# configWindowThread()
-
 # --------------create mainWindow--------------------
-
 # mainWindowThread = threading.Thread(target=createMainWindow)
 # mainWindowThread.start()
-
-# createMainWindow()
-# mainWindow.hide()
 
 # ----------pystray----------
 # image = Image.open("threeLines.png")
@@ -276,8 +196,8 @@ dc.rectangle([(0, height/5*3), (width, height/5*4)], fill=(120, 120, 120))
 menu = (MenuItem("Show Window", showMainWindow), MenuItem("Exit", end))
 icon = pystray.Icon("ApplicationTimeTracker", image,
                     "ApplicationTimeTracker", menu)
-# loop
+
 loopThread = threading.Thread(target=loop, name="loop-Thread")
 loopThread.start()
-# start trayIcon
+
 icon.run()
